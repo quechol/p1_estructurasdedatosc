01ed4c1bb12ed7136018932d950bbcdaedb2b343
quechol <41605691+quechol@users.noreply.github.com>
2019-01-20 13:40:03 -0600

    Update Practica1-Estructuras.c

--- Practica1-Estructuras.c
+++ Practica1-Estructuras.c
@@ -1,48 +1,67 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-struct registro{
-	int id;
-	char nombre[30];
-	char direccion[30];
-	char numero[10];
-	char gen[1];
-}arreglo[50];
-	
-void abrir (){
-	
-}
-void agregar(){
-	
-}
+typedef struct {
+	char *nombre;
+	int edad;
+}personas;
 
-void consultas(){
-	
-}
+personas *per;
+
+void vaciar(char temp[]);
+void copiar(char temp[], int i);
+
 
 int main (){
 	
-	FILE *archivo;
-	int op=0,i=0,j=0;
+	int i,j;
+	int op;
+	char aux;
+	char temp[50];
+	int cont=0;
 	
-	/*
-	struct registro registros[10];
-	int i=0;
-	while (i < 10){
-		printf("Ingresa el registro %i : ",i+1);
-		fgets(registros[i].nombre,30,stdin);
-		registros[i].id = i+1;
-		i++;
+	FILE *f;
+	f = fopen("registros.txt","r+");
+	if (f == NULL){
+		printf("No se ha podido abrir el fichero \n");
+		f = fopen("registros.txt","w+");
+		printf("El archivo se a creado automaticamente");
+		//exit(1);
+	}
+	while (!feof(f)){
+		fgets(temp,50,f);
+		cont++;
 	}
 	
+	rewind (f);
 	
-	strcpy(arreglo[0].direccion, "14 SUR");
-	strcpy(arreglo[0].gen, "M");
-	arreglo[0].id = 101;
-	strcpy(arreglo[0].nombre, "Saul");
-	strcpy(arreglo[0].numero, "2221234567");
-	*/
+	per = (personas*)malloc(cont*sizeof(personas));
+	if (per == NULL){
+		printf("No se ha podido reservar ma memoria. \n");
+		exit(1);
+	}
 	
+	for(i=0; !feof(f); i++){
+		vaciar(temp);
+		aux = '0';
+		for (j=0; aux != '-'; j++){
+			aux = fgetc(f);
+			if (aux != '-'){
+				temp[j]= aux;
+			}	
+		
+		}
+		copiar (temp,i);
+		
+		fgets (temp,4,f);
+		per[i].edad = atoi(temp); //convertir a enteros
+		
+		printf("Nombre: %s Edad: %i. \n ",per[i].nombre, per[i].edad);
+	}
+	
+	//menu
+	printf ("\n------------------------\n");
 	do{
 		printf("Bienvenido");
 		printf("\n 1.- Registrar Persona.");
@@ -77,9 +96,23 @@ int main (){
 		
 	}while (op!=0);
 	
-	/*archivo = fopen("registros.txt","w");
-	if (archivo){
-		fwrite(registros,sizeof(struct registro),10,archivo);
-		fclose(archivo);
-	}*/
 }
+
+void vaciar(char temp[]){
+	int i;
+	for (i =0 ; i<50; i++){
+		temp[i] = '\0';
+	}
+}
+
+void copiar(char temp[], int i){
+	int N = strlen(temp) + 1;
+	per[i].nombre = (char*)malloc(N*sizeof(char));
+	if (per[i].nombre == NULL){
+		printf("ERROR \n");
+		exit(1);
+	}
+	strcpy(per[i].nombre, temp);
+}
+
+
